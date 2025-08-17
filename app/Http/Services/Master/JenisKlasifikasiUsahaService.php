657b69252da811fbe1860bf0a0d0728fb7a0c10f
pepypeppep <pepy.peppep@gmail.com>
2025-08-17 13:19:44 +0700

    Implement klasifikasi usaha

--- app/Http/Services/Master/JenisKlasifikasiUsahaService.php
+++ app/Http/Services/Master/JenisKlasifikasiUsahaService.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Services\Master;
 
-use App\Enums\RoleEnum;
-use App\Models\MasterJenisIkan;
+use App\Models\MasterKlasifikasiUsaha;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
-use Illuminate\Container\AttributesDB;
 use Yajra\DataTables\Facades\DataTables;
 
-class JenisIkanService
+class JenisKlasifikasiUsahaService
 {
     /* Get alls */
     public function getAll()
     {
-        $data = MasterJenisIkan::orderBy('name');
+        $data = MasterKlasifikasiUsaha::orderBy('name');
 
         return DataTables::eloquent($data)
             ->addIndexColumn()
@@ -22,18 +19,18 @@ class JenisIkanService
                 $btnEdit = '';
                 $btnDelete = '';
                 // Btn Edit
-                if (auth()->user()->can('jenis-perairan.update')) {
+                if (auth()->user()->can('jenis-klasifikasi-usaha.update')) {
                     $btnEdit = '<button href="javascript:void(0);" title="Edit data pengguna" id="btn-modal-edit-user"
-                        data-id="' . $row->id . '"  data-url-action="' . route('master.jenis-perairan.update', $row->id) . '" data-url-get="' . route('master.jenis-perairan.edit', $row->id) . '"
+                        data-id="' . $row->id . '"  data-url-action="' . route('master.jenis-klasifikasi-usaha.update', $row->id) . '" data-url-get="' . route('master.jenis-klasifikasi-usaha.edit', $row->id) . '"
                         class="items-center justify-center size-[37.5px] p-0 text-white btn bg-yellow-500 border-yellow-500 hover:text-white hover:bg-yellow-600 hover:border-yellow-600 focus:text-white focus:bg-yellow-600 focus:border-yellow-600 focus:ring focus:ring-yellow-100 active:text-white active:bg-yellow-600 active:border-yellow-600 active:ring active:ring-yellow-100 dark:ring-yellow-400/20">
                         <i class="ri-edit-line"></i>
                         </button>';
                 }
 
                 // Btn Delete
-                if (auth()->user()->can('jenis-perairan.delete')) {
+                if (auth()->user()->can('jenis-klasifikasi-usaha.delete')) {
                     $btnDelete = '<button href="javascript:void(0);" title="Hapus data pengguna" id="btn-delete-user" onclick="confirmDelete(this)"
-                        data-id="' . $row->id . '"  data-url-action="' . route('master.jenis-perairan.destroy', $row->id) . '"
+                        data-id="' . $row->id . '"  data-url-action="' . route('master.jenis-klasifikasi-usaha.destroy', $row->id) . '"
                         class="items-center justify-center size-[37.5px] p-0 text-white btn bg-red-500 border-red-500 hover:text-white hover:bg-red-600 hover:border-red-600 focus:text-white focus:bg-red-600 focus:border-red-600 focus:ring focus:ring-red-100 active:text-white active:bg-red-600 active:border-red-600 active:ring active:ring-red-100 dark:ring-red-400/20">
                         <i class="ri-delete-bin-line"></i>
                         </button>';
@@ -48,7 +45,7 @@ class JenisIkanService
     /* Get data by ID */
     public function getById(int $id)
     {
-        $data = MasterJenisIkan::findOrFail($id);
+        $data = MasterKlasifikasiUsaha::findOrFail($id);
 
         return $data;
     }
@@ -59,17 +56,17 @@ class JenisIkanService
         try {
             // DB Transaction
             DB::beginTransaction();
-            $data = MasterJenisIkan::create([
+            $data = MasterKlasifikasiUsaha::create([
                 'name' => $attributes['name']
             ]);
 
             // Return success response
             DB::commit();
-            return redirect()->back()->with('success', 'Jenis perairan berhasil ditambahkan');
+            return redirect()->back()->with('success', 'Jenis klasifikasi-usaha berhasil ditambahkan');
         } catch (\Exception $e) {
             // Return error response
             DB::rollBack();
-            return redirect()->back()->withInput()->withErrors(['error' => 'Jenis perairan gagal ditambahkan. Error :' . $e->getMessage()]);
+            return redirect()->back()->withInput()->withErrors(['error' => 'Jenis klasifikasi-usaha gagal ditambahkan. Error :' . $e->getMessage()]);
         }
     }
 
@@ -81,7 +78,7 @@ class JenisIkanService
             DB::beginTransaction();
 
             // Get data
-            $data = MasterJenisIkan::findOrFail($id);
+            $data = MasterKlasifikasiUsaha::findOrFail($id);
             // Update data data
             $data->update([
                 'name' => $attributes['name'] ?? $data->name
@@ -89,11 +86,11 @@ class JenisIkanService
 
             // Return success response
             DB::commit();
-            return redirect()->back()->with('success', 'Jenis perairan berhasil diperbarui');
+            return redirect()->back()->with('success', 'Jenis klasifikasi-usaha berhasil diperbarui');
         } catch (\Exception $e) {
             // Return error response
             DB::rollBack();
-            return redirect()->back()->withInput()->withErrors(['error' => 'Jenis perairan gagal diperbarui. Error :' . $e->getMessage()]);
+            return redirect()->back()->withInput()->withErrors(['error' => 'Jenis klasifikasi-usaha gagal diperbarui. Error :' . $e->getMessage()]);
         }
     }
 
@@ -105,16 +102,16 @@ class JenisIkanService
             DB::beginTransaction();
 
             // Get data
-            $data = MasterJenisIkan::findOrFail($id);
+            $data = MasterKlasifikasiUsaha::findOrFail($id);
             $data->delete();
 
             // Return success response
             DB::commit();
-            return redirect()->route('master.jenis-perairan.index')->with('success', 'Jenis perairan berhasil dihapus');
+            return redirect()->route('master.jenis-klasifikasi-usaha.index')->with('success', 'Jenis klasifikasi-usaha berhasil dihapus');
         } catch (\Exception $e) {
             // Return error response
             DB::rollBack();
-            return redirect()->back()->withErrors(['error' => 'Jenis perairan gagal dihapus. Error :' . $e->getMessage()]);
+            return redirect()->back()->withErrors(['error' => 'Jenis klasifikasi-usaha gagal dihapus. Error :' . $e->getMessage()]);
         }
     }
 }
---
+++ resources/views/admin/masters/jenis-klasifikasi-usaha/partials/modal-add.blade.php
@@ -0,0 +1,34 @@
+<div id="modal-add" modal-center
+    class="fixed flex flex-col hidden transition-all duration-300 ease-in-out left-2/4 z-drawer -translate-x-2/4 -translate-y-2/4 show">
+    <div class="w-screen md:w-[40rem] bg-white shadow rounded-md dark:bg-zink-600 flex flex-col h-full">
+        <div class="flex items-center justify-between p-4 border-b border-slate-200 dark:border-zink-500">
+            <h5 class="text-16">Tambah Jenis KlasifikasiUsaha Baru</h5>
+            <button data-modal-close="modal-add"
+                class="transition-all duration-200 ease-linear text-slate-500 hover:text-red-500 dark:text-zink-200 dark:hover:text-red-500"><i
+                    data-lucide="x" class="size-5"></i></button>
+        </div>
+        <form action="{{ route('master.jenis-klasifikasi-usaha.store') }}" method="POST">
+            @csrf
+            {{-- Start Modal Body --}}
+            <div class="max-h-[calc(theme('height.screen')_-_180px)] p-4 overflow-y-auto">
+                {{-- Nama --}}
+                <div class="mb-1">
+                    <label for="" class="inline-block mb-2 text-base font-medium">Nama <strong
+                            class="text-red-500">*</strong></label>
+                    <input type="text" id="name" name="name"
+                        class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                        placeholder="Masukkan nama jenis klasifikasi usaha" required>
+                </div>
+            </div>
+            {{-- End Modal Body --}}
+            {{-- Start Modal Footer --}}
+            <div class="flex items-center justify-between p-4 mt-auto border-t border-slate-200 dark:border-zink-500">
+                <button type="submit"
+                    class="btn bg-custom-500 text-white hover:bg-custom-600 focus:bg-custom-600 w-full">
+                    <i class="ri-save-line"></i> Simpan
+                </button>
+            </div>
+            {{-- End Modal Footer --}}
+        </form>
+    </div>
+</div>
