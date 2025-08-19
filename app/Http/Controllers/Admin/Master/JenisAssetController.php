888b6ba90f3b407c7cb5eaa30a865a7c25ee87af
adamarnap <adamarnap0117@gmail.com>
2025-08-19 20:55:03 +0700

    Implement Master > Jenis Asset

--- app/Http/Controllers/Admin/Master/JenisAssetController.php
+++ app/Http/Controllers/Admin/Master/JenisAssetController.php
@@ -2,42 +2,40 @@
 
 namespace App\Http\Controllers\Admin\Master;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Services\Master\JenisAssetService;
+use App\Http\Requests\Master\JenisAsset\CreateRequest;
+use App\Http\Requests\Master\JenisAsset\UpdateRequest;
 
 class JenisAssetController extends Controller
 {
+    public function __construct(protected JenisAssetService $jenisAssetService) {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $this->setRule('jenis-asset.read');
-        return view('admin.masters.jenis-asset.index');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // Get data jenis penyuluhan for data table
+        if (request()->ajax()) {
+            return $this->jenisAssetService->getAll();
+        }
+
+        return view('admin.masters.jenis-asset.index');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateRequest $request)
     {
-        //
-    }
+        $this->setRule('jenis-asset.create');
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        // Store process
+        return $this->jenisAssetService->store($request->validated());
     }
 
     /**
@@ -45,22 +43,27 @@ class JenisAssetController extends Controller
      */
     public function edit(string $id)
     {
-        //
-    }
+        $this->setRule('jenis-asset.update');
 
+        return $this->jenisAssetService->getById($id);
+    }
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request, $id)
     {
-        //
+        $this->setRule('jenis-asset.update');
+        // Update process
+        return $this->jenisAssetService->update($id, $request->validated());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->setRule('jenis-asset.delete');
+        // Delete Process
+        return $this->jenisAssetService->delete($id);
     }
 }
