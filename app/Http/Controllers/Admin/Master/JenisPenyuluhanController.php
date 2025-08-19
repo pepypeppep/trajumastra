584e1f551cd230a5aa90246cc5177b1b6d5c91aa
adamarnap <adamarnap0117@gmail.com>
2025-08-19 20:18:54 +0700

    Implement Master > Jenis Penyuluhan

--- app/Http/Controllers/Admin/Master/JenisPenyuluhanController.php
+++ app/Http/Controllers/Admin/Master/JenisPenyuluhanController.php
@@ -2,42 +2,40 @@
 
 namespace App\Http\Controllers\Admin\Master;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Services\Master\JenisPenyuluhanService;
+use App\Http\Requests\Master\JenisPenyuluhan\CreateRequest;
+use App\Http\Requests\Master\JenisPenyuluhan\UpdateRequest;
 
 class JenisPenyuluhanController extends Controller
 {
+    public function __construct(protected JenisPenyuluhanService $rangePenghasilanService) {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $this->setRule('jenis-penyuluhan.read');
-        return view('admin.masters.jenis-penyuluhan.index');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // Get data jenis penyuluhan for data table
+        if (request()->ajax()) {
+            return $this->rangePenghasilanService->getAll();
+        }
+
+        return view('admin.masters.jenis-penyuluhan.index');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateRequest $request)
     {
-        //
-    }
+        $this->setRule('jenis-penyuluhan.create');
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        // Store process
+        return $this->rangePenghasilanService->store($request->validated());
     }
 
     /**
@@ -45,22 +43,27 @@ class JenisPenyuluhanController extends Controller
      */
     public function edit(string $id)
     {
-        //
-    }
+        $this->setRule('jenis-penyuluhan.update');
 
+        return $this->rangePenghasilanService->getById($id);
+    }
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request, $id)
     {
-        //
+        $this->setRule('jenis-penyuluhan.update');
+        // Update process
+        return $this->rangePenghasilanService->update($id, $request->validated());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->setRule('jenis-penyuluhan.delete');
+        // Delete Process
+        return $this->rangePenghasilanService->delete($id);
     }
 }
