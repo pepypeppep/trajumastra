9175d074d56e93bd776d844f6742f80855a3b783
adamarnap <adamarnap0117@gmail.com>
2025-08-19 19:33:12 +0700

    Implement Master > Asset Digunakan

--- app/Http/Controllers/Admin/Master/AssetDigunakanController.php
+++ app/Http/Controllers/Admin/Master/AssetDigunakanController.php
@@ -2,42 +2,40 @@
 
 namespace App\Http\Controllers\Admin\Master;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Services\Master\AssetDigunakanService;
+use App\Http\Requests\Master\AssetDigunakan\CreateRequest;
+use App\Http\Requests\Master\AssetDigunakan\UpdateRequest;
 
 class AssetDigunakanController extends Controller
 {
+    public function __construct(protected AssetDigunakanService $assetDigunakanService) {}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $this->setRule('asset-digunakan.read');
-        return view('admin.masters.asset-digunakan.index');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        // Get data jenis ikan for data table
+        if (request()->ajax()) {
+            return $this->assetDigunakanService->getAll();
+        }
+        
+        return view('admin.masters.asset-digunakan.index');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateRequest $request)
     {
-        //
-    }
+        $this->setRule('asset-digunakan.create');
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        // Store process
+        return $this->assetDigunakanService->store($request->validated());
     }
 
     /**
@@ -45,22 +43,27 @@ class AssetDigunakanController extends Controller
      */
     public function edit(string $id)
     {
-        //
-    }
+        $this->setRule('asset-digunakan.update');
 
+        return $this->assetDigunakanService->getById($id);
+    }
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateRequest $request, $id)
     {
-        //
+        $this->setRule('asset-digunakan.update');
+        // Update process
+        return $this->assetDigunakanService->update($id, $request->validated());
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->setRule('asset-digunakan.delete');
+        // Delete Process
+        return $this->assetDigunakanService->delete($id);
     }
 }
