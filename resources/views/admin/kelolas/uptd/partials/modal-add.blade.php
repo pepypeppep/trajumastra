24bf487441a4507b6a622645f84557d9169fd621
pepypeppep <pepy.peppep@gmail.com>
2025-10-20 07:59:49 +0700

    Fix input form at bbi

--- resources/views/admin/kelolas/uptd/partials/modal-add.blade.php
+++ resources/views/admin/kelolas/uptd/partials/modal-add.blade.php
@@ -19,6 +19,14 @@
                         class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                         placeholder="Masukkan nama" required>
                 </div>
+                {{-- Telepon --}}
+                <div class="mt-3">
+                    <label for="" class="inline-block mb-2 text-base font-medium">Telepon <strong
+                            class="text-red-500">*</strong></label>
+                    <input type="text" id="phone" name="phone"
+                        class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                        placeholder="Masukkan Nomor Telepon" required>
+                </div>
                 {{-- Jenis Ikan --}}
                 <div class="mt-3">
                     <label for="" class="inline-block mb-2 text-base font-medium">Jenis Ikan <strong
